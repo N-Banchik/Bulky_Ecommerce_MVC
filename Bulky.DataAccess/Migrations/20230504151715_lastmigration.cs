1a36ea0216a43e1cf596a8cff187aa52483e0b16
N-Banchik <87769833+N-Banchik@users.noreply.github.com>
2023-05-04 18:17:39 +0300

    last push

---
+++ Bulky.DataAccess/Migrations/20230504151715_lastmigration.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class lastmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\91d4d68c-df73-460e-852f-c3a5a95ab58f.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\b00e17b6-2b75-4325-b8c1-d8aa20f053d5.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\a613d850-2772-4650-a518-8ac1582ee8d0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\6e74fdf6-3cd9-4881-8319-0f2f53e07c07.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\0f4304d9-c3e9-4826-a576-574a1f6619a8.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\7fdfaaef-b424-4e3d-8149-18e8f42516e3.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\86d697b5-6932-44eb-9a26-edf6c6e20b2a.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "\\Images\\Product\\1c73a674-43ed-497a-ba0f-d1c013191a3a.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "");
+        }
+    }
+}
